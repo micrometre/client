02fb541ff56e2c4cb626d0de038d29953586c2b3
Henok <henokmicrometre@gmail.com>
2022-04-23 20:27:35 +0100

    added dynamic

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick .`,
+    author: `@micrometreuk`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    menuLinks: [
+      {
+        name: "home",
+        link: "/",
+      },
+      {
+        name: "page2",
+        link: "/page-2",
+      },
+    ],
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/site-data/`,
+      },
+    },
+  ],
+}
---
+++ src/pages/test.js
@@ -0,0 +1,10 @@
+import React from "react"
+import MotoPage from "../components/moto"
+function AboutPage(props) {
+  return (
+    <div className="about-container">
+      <MotoPage />
+    </div>
+  )
+}
+export default AboutPage
